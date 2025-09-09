27ea3eb1dfa7fa27e861a0f0ff6af687dccac56a
Santiago Pampillon <120607419+QuaaDev@users.noreply.github.com>
2025-09-09 04:37:59 -0300

    Se agrego movimiento vertical
    Si las unidades no estan en combate, se moveran de forma vertical para buscar nuevos objetivos

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -1,9 +1,14 @@
 extends Node2D
 @onready var deteccion_mouse: Area2D = $"Deteccion Mouse"
 
-const GRID_SIZE := 10  # Tamaño de la cuadrícula (10x10)
-const CELL_SIZE := 50  # Tamaño de cada celda (debe coincidir con el de la serpiente)
+@export var GRID_SIZE := 10  # Tamaño de la cuadrícula (10x10)
+@export var CELL_SIZE := 50  # Tamaño de cada celda (debe coincidir con el de la serpiente)
 
+var diccionario_cuadricula_movimientos : Dictionary
+#Diccionario que almacena una cuadricula del mapa actual, la clave es el Vector2 
+#de la cuadricula, el dato es un booleano que almacena si esa casilla esta ocupada o no
+#{Vector2(1,1) : false, 1: true} 
+#True si la casilla esta libre, false si esta ocupada
 func _draw() -> void:
 	draw_grid()
 
@@ -14,7 +19,6 @@ func draw_grid() -> void:
 	for x in range(GRID_SIZE + 1):
 		var x_pos = x * CELL_SIZE
 		draw_line(Vector2(x_pos, 0), Vector2(x_pos, GRID_SIZE * CELL_SIZE), color, 2)
-
 	# Dibujar líneas horizontales
 	for y in range(GRID_SIZE + 1):
 		var y_pos = y * CELL_SIZE
@@ -22,3 +26,37 @@ func draw_grid() -> void:
 
 func _ready() -> void:
 	queue_redraw()  # Redibujar la cuadrícula cuando se carg
+	crear_diccionario_del_mapa(diccionario_cuadricula_movimientos)
+
+func crear_diccionario_del_mapa(diccionario_a_llenar : Dictionary) -> void:
+	#Crea y carga el diccionario con valores
+	for y in GRID_SIZE+1: #Eje y
+		for x in GRID_SIZE+1:#Eje x
+			diccionario_a_llenar[Vector2(x,y)] = true
+			
+func solicitar_movimiento(posicion : Vector2) -> bool:
+	#-----------importante-------------
+	#Esta funcion debe de recibir siempre la posicion a la que quiere ir, no la actual
+	#-----------importante-------------
+	#Convierte la posicion global a la posicion local que utiliza el diccionario
+	#Divide la posicion en pixeles por el tamaño de las cuadriculas
+	var posicion_local = Vector2((posicion.x / CELL_SIZE),(posicion.y/CELL_SIZE))
+	#Si la posicion local supera los limites del grid, se cancela el movimiento
+	if (posicion_local.x > GRID_SIZE or posicion_local.x < 0) or (posicion_local.y >= GRID_SIZE or posicion_local.y < 0):
+		return false
+	#Si la posicion del diccionario esta libre (true), vuelve valido el movimiento y marca la casilla
+	#como ocupada (false)
+	elif diccionario_cuadricula_movimientos[posicion_local] == true:
+		actualizar_diccionario_movimiento(posicion_local,false)
+		return true
+	#Si la casilla esta ocupada (false), cancela el movimiento
+	else:
+		#print("PATATA")
+		return false
+
+#Actualiza el diccionario con nueva informacion
+func actualizar_diccionario_movimiento(posicion : Vector2, valor : bool) -> void:
+	diccionario_cuadricula_movimientos[posicion] = valor
+func coso():
+	#print(diccionario_cuadricula_movimientos)
+	pass
--- Scripts/soldado_basico.gd
+++ Scripts/soldado_basico.gd
@@ -5,14 +5,15 @@ class_name soldado_basico
 @onready var detectar_enemigo: RayCast2D = $DetectarEnemigo
 @onready var cd_ataque: Timer = $cd_ataque
 @onready var animacion_recibir_daño: Timer = $animacion_recibir_daño
-
+@onready var grid = self.get_parent() #Referencia al grid
 #----------Referencias--------------
 var estoy_seleccionado := false
-var speed := 3
+var speed := 1
 var direction := Vector2.ZERO
 var cell_size := 50 #Tamaño de la cuadricula sobre la cual nos queremos mover
 var is_moving := false
 var color_original : Color
+var cantidad_a_mover : int
 @export var aliado := true
 @export var vida : int = 10
 @export var ataque : int = 5
@@ -20,6 +21,7 @@ var color_original : Color
 @export var rango_ataque : int = 1
 @export var cd_ataque_variable : float = 1.0
 @export var sprite_unidad_path : String
+@export var movimiento_automatico : bool = true
 
 func _ready() -> void:
 	input_pickable = true #Hace que siempre sea seleccionado por el mouse
@@ -35,6 +37,7 @@ func _ready() -> void:
 	animacion_recibir_daño.timeout.connect(_on_animacion_recibir_daño_timeout)
 	cd_ataque.timeout.connect(cd_ataque_timeout)
 	self.input_event.connect(_on_input_event)
+	grid.ready.connect(grid_esta_ready)
 	#-----------Conectar señales---------
 	#Cambia el collision_layer para diferenciar aliados de enemigos
 	#Cambia la collision_mask para detectar siempre a los enemigos de su bando
@@ -42,18 +45,23 @@ func _ready() -> void:
 	#(tamaño de las celdas * cantidad de celdas que puede atacar)
 	#Cambia el color del canvas 
 	#Almacena su color original
+	#Define cuantos pixeles se movera (es temporal, mas adelante sera mas complejo)
 	if aliado: 
 		collision_layer = 1
 		detectar_enemigo.collision_mask = 2
 		detectar_enemigo.target_position = Vector2(0,-(cell_size*rango_ataque))
 		modulate = Color("Blue")
 		color_original = Color("Blue")
+		cantidad_a_mover = -cell_size
 	else:
 		collision_layer = 2
 		detectar_enemigo.collision_mask = 1
 		detectar_enemigo.target_position = Vector2(0,(cell_size*rango_ataque)) 
 		modulate = Color("Green")
 		color_original = Color("Green")
+		cantidad_a_mover = cell_size
+	#Informa al grid sobre mi posicion actual
+	#posicion_local_al_grid = Vector2((position.x / grid.CELL_SIZE),(position.y/grid.CELL_SIZE))
 
 func _physics_process(delta):
 	#Si el raycast detecta una colision y el ataque esta disponible, ejecuta el ataque
@@ -61,6 +69,19 @@ func _physics_process(delta):
 		detectar_enemigo.enabled = false
 		cd_ataque.start(cd_ataque_variable)
 		detectar_enemigo.get_collider().perder_vida(ataque)
+	#Si el movimiento automatico esta activo, ejecuta el codigo
+	elif movimiento_automatico and detectar_enemigo.enabled:
+		if !is_moving: #Si ya esta en movimiento, no agregara nuevos movimientos
+			if grid.solicitar_movimiento(Vector2((position.x),(position.y + cantidad_a_mover))):
+				#Si el movimiento es valido, ejecuta el movimiento
+				#Segun su bando va hacia arriba o abajo
+				if aliado:
+					direction = Vector2.UP
+				else:
+					direction = Vector2.DOWN
+				#Actualiza su anterior posicion en el grid para que figure como libre
+				grid.actualizar_diccionario_movimiento(Vector2((position.x / grid.CELL_SIZE),(position.y/grid.CELL_SIZE)) , true)
+				move_to_next_tile() #Ejecuta el movimiento
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	#Si este objeto en particular detecta el click del mouse, ejecuta el codigo
@@ -74,7 +95,7 @@ func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> voi
 func _input(event: InputEvent) -> void:
 	if estoy_seleccionado: #Activa el movimiento si se selecciona esta unidad
 		if is_moving:
-			return
+			return #<------------ CUIDADO CON ESTE HIJUEPUTA
 		#Movimiento basico del boneco
 		if event is InputEventKey and event.pressed and !is_moving:
 			if event.keycode == KEY_UP:
@@ -125,7 +146,10 @@ func get_vida() -> int:
 func set_vida(x : int) -> void:
 	#Si la vida es menor a 1, se elimina el objeto
 	if x <= 0:
+		#Al morir, deja su posicion como libre
+		grid.actualizar_diccionario_movimiento(Vector2((position.x / grid.CELL_SIZE),(position.y/grid.CELL_SIZE)) , true)
 		queue_free()
+		
 	else:
 		vida = x
 	
@@ -147,7 +171,13 @@ func set_armadura(x : int) -> void:
 func cd_ataque_timeout() -> void:
 	#Al acabar el cd activa el raycast para realizar ataques
 	detectar_enemigo.enabled = true
+	detectar_enemigo.force_raycast_update() #Fuerza el update del raycast para evitar frames sin escanear
 
 
 func _on_animacion_recibir_daño_timeout() -> void:
 	modulate = color_original
+
+#Cuando el grid termina su ready, este envia la informacion sobre su ubicacion actual
+#Para actualizar el diccionario
+func grid_esta_ready() -> void:
+	grid.actualizar_diccionario_movimiento(Vector2((position.x / grid.CELL_SIZE),(position.y/grid.CELL_SIZE)) , false)
